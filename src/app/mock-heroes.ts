d9c29429e9c05c192d7234d58d6b4bb6d11d960a
EsauKip <esau.kiprono@student.moringaschool.com>
2022-07-15 11:02:15 +0300

    display a list of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 12, name: "Mr. Nice", date: "March 15, 2018" },
+    { id: 13, name: "Narco", date: "March 15, 2018" },  
+    { id: 14, name: "Bombasto", date: "March 15, 2018" },
+    { id: 15, name: "Celeritas", date: "March 15, 2018" },
+    { id: 16, name: "Magneta", date: "March 15, 2018" },
+    { id: 17, name: "RubberMan", date: "March 15, 2018" },
+    { id: 18, name: "Dynama", date: "March 15, 2018" },
+    { id: 19, name: "Dr IQ", date: "March 15, 2018" },
+    { id: 20, name: "Magma", date: "March 15, 2018" },
+    { id: 21, name: "Tornado", date: "March 15, 2018" }
+];
