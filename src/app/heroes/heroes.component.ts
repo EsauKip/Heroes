3531a9047fa0c641b237ecb7fce4109af06100f6
EsauKip <esau.kiprono@student.moringaschool.com>
2022-07-15 10:15:02 +0300

    add forms input and uss property binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import{Hero} from '../hero';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,7 +10,8 @@ export class HeroesComponent implements OnInit {
 
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    name: 'Windstorm',
+    date: 'March 15, 2018'
   };
 
   constructor() { }
