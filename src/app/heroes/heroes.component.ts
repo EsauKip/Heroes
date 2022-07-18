3af528d08d6ab51020fcafeca1c69231b175092d
EsauKip <esau.kiprono@student.moringaschool.com>
2022-07-18 15:07:57 +0300

    add a service and inject in the common component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,29 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import{Hero} from '../hero';
 import{HEROES} from '../mock-heroes';
-
+import{HeroService} from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-    // date: 'March 15, 2018'
-    ,
-    date: ''
-  };
+hero:Hero[]=[];
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  //   // date: 'March 15, 2018'
+  //   ,
+  //   date: ''
+  // };
   heroes = HEROES;
   selectedHero?: Hero;
 
-  constructor() {
+  constructor(private heroService: HeroService) {
+    
 
    }
+   getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
 
   ngOnInit(): void {
+    this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
